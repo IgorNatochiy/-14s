7faafc8f67b03b52bddc57a9a4260f6d9b61b1ec
Игорь Наточий <igornatochiy96@mail.ru>
2022-08-02 19:41:30 +0600

    Добавьте файлы проекта.

---
+++ Задача 14/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_14
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+            }
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+
+            public Animal(string name)
+            {
+                Name = name;
+            }
+            public abstract void Say();
+
+            public void ShowInfo()
+            {
+                Console.WriteLine(Name);
+                Say();
+            }
+        }
+        class Cat : Animal
+
+        {
+            string say;
+            string name;
+            public override string Name
+            {
+                get => name;
+                set => name = value;
+            }
+            public Cat(string name, string say)
+                : base(name)
+            {
+                this.say = say;
+            }
+            public override void Say()
+            {
+                Console.WriteLine(say);
+                Console.ReadKey();
+            }
+        }
+        class Dog : Animal
+        {
+            private string name;
+            public override string Name
+            {
+                get => name;
+                set => name = value;
+            }
+            public Dog(string name, string say)
+                : base(name)
+            {
+            }
+            public override void Say()
+            {
+                Console.WriteLine("Гав");
+                Console.ReadKey();
+            }
+        }
+    }
+} 
